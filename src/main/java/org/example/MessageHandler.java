79f84eeb1d0ad4aa6bc8c7ace11edfe1198a336c
vidergach <vik709751@gmail.com>
2025-10-05 17:49:57 +0500

    Add files via upload
    Дописала метолы для команд /add,/tasks,/delete

--- src/main/java/org/example/MessageHandler.java
+++ src/main/java/org/example/MessageHandler.java
@@ -1,22 +1,80 @@
 package org.example;
 
-public class MessageHandler {
-    public String processUserInput(String userInput, String userId) {
-    System.out.println("сообщение: " + userInput + " от: " + userId);
-    String outputText = processCommand(userInput);
-    System.out.println("Ответ: " + outputText);
-    return outputText;
-    }
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    private String processCommand(String userInput) {
+public class MessageHandler {
+    private final List<String> tasks = new ArrayList<>();//надо сделать
+    private final List<String> completedTasks = new ArrayList<>();//сделано задач
+
+    public String processUserInput(String userInput, String userId) {
+
+        System.out.println("сообщение: " + userInput + " от: " + userId);
+        String outputText = Response(userInput);
+        System.out.println("Ответ: " + outputText);
+        return outputText;
+    }
+    private String Response(String userInput){
         if ("/start".equals(userInput)) {
             return startMessage();
         } else if ("/help".equals(userInput)) {
             return helpMessage();
+        } else if(userInput.startsWith("/add")){
+            return addTask(userInput);
+        } else if("/tasks".equals(userInput)){
+            return showTasks();
+        } else if(userInput.startsWith("/delete")){
+            return deleteTask(userInput);
+        } else {
+            return "Неизвестная команда.\n" +
+                    "Введите /help для просмотра доступных команд.";
         }
-        return userInput;
     }
 
+    private String addTask(String userInput) {
+        if (userInput.length() <= 5) {
+            return "Упс\uD83D\uDE05, похоже вы " +
+                    "забыли указать задачу после команды /add \n" +
+                    "Например: /add Полить цветы";
+        }
+        String task = userInput.substring(5).trim();
+        if (task.isEmpty()) {
+            return "Задача не может быть пустой!";
+        }
+
+        // Проверка на существующую задачу
+        if (tasks.contains(task)) {
+            return "Задача \"" + task + "\" уже есть в списке!";
+        }
+
+        tasks.add(task);
+        return "Задача \"" + task + "\" добавлена!";
+    }
+
+    private String showTasks() {
+        if (tasks.isEmpty())
+            return "Список задач пуст!";
+        String list_tasks = "Вот список ваших задач:\n";
+        for (int i = 0; i < tasks.size(); i++) {
+            list_tasks += "  " + (i + 1) + ". " + tasks.get(i) + "\n";
+        }
+        return list_tasks;
+    }
+
+    private String deleteTask(String userInput) {
+        if (userInput.length() <= 8) {
+            return "Упс\uD83D\uDE05, похоже вы забыли указать задачу после команды /delete.\n" +
+                    "Например: /delete Полить цветы";
+        }
+        String task = userInput.substring(8).trim();
+        if (!tasks.contains(task)) {
+            return "Задача \"" + task + "\" не найдена в списке!";
+        }
+        tasks.remove(task);
+        return "🗑️ Задача \"" + task + "\" удалена из списка задач!";
+    }
     private String startMessage() {
         return "Добро пожаловать в планировщик задач! \uD83D\uDC31 📝 \n" +
                 "Я могу организовывать ваши задачи.\n" +
